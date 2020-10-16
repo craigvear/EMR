641d7a6e3f22b329c12d755fd34ab665f54f19c5
craigv <vear2ev2.co.uk>
2020-10-16 11:34:10 +0100

    ML_test_with threading_and_audio

--- ML_robot_move.py
+++ ML_robot_move.py
@@ -3,7 +3,7 @@ from random import shuffle, randrange
 import pandas as pd
 from numpy import reshape, array
 from time import sleep, time
-from jetbot import Robot
+# from jetbot import Robot
 from threading import Thread, Lock, Event
 
 from pydub import AudioSegment
@@ -18,16 +18,13 @@ BODY_model_path = 'training/models/LSTM_Bidirectional_64x4_no_lookback_body_full
 BODY_model = load_model(BODY_model_path)
 test_dataset_path = 'training/good_dataset_mini.csv'
 
-# audio source variables:
-# 2) single audio file
-my_audio_file = (data/test.wav)
-audio = AudioSegment.from_file(my_audio_file)
+# audio source variables
+my_audio_file = ('data/bill_evans_intro.wav')
+audio = AudioSegment.from_wav(my_audio_file)
 audio_len = audio.duration_seconds
+print (f'length of audio = {audio_len} seconds')
 
 # defines the readable dataset, shuffles it, then resets index
-running = True
-
-# mix up the original source dataset for reading
 df = pd.read_csv(test_dataset_path, sep=",", header=None, names=["id", "limb", "x", "y", "z", "freq", "amp"])
 df = df.filter(['x', 'y', 'z'])
 print(df)
@@ -38,20 +35,19 @@ df = df.iloc[index]
 df.reset_index()
 
 # functions producing data
-def ml_predictions(features):
+def ml_predictions(features): # RNN predict x, y, z
     row = array(features)
     inputX = reshape(row, (row.shape[0], row.shape[1], 1))
     pred = BODY_model.predict(inputX, verbose=0)
-    pred_x, pred_y = pred[0,0], pred [0,1]
+    pred_x, pred_y = pred[0,0], pred [0,1] # only want x and y at this stage
     # print('prediction RNN = ', pred)
-    return pred_x, pred_y
+    return (pred_x, pred_y)
 
-def what_is_duration():
+def what_is_duration(): # of the sound/movment event
     dur_rnd = random() * 2
     return dur_rnd
 
-def choose_incoming_df():
-    # randomly find a row and use as seed
+def choose_incoming_df(): # randomly find a row and use as seed
     ind = randrange(index_len)
     seed = [[df['x'][ind], df['y'][ind], df['z'][ind]]]
     return seed
@@ -61,47 +57,27 @@ def producer_data(out_q):
     while running:
         dur = what_is_duration()
         seed = choose_incoming_df()
-        move = ml_predictions(seed)
+        pred_x, pred_y = ml_predictions(seed)
 
         # Make an (data, event) list and hand it to the consumer
         evt = Event()
-        out_q.put((dur, seed, move, evt))
+        out_q.put((dur, seed, pred_x, pred_y, evt))
 
         # Wait for the consumer to process the item
         evt.wait()
 
 # functions that consume data
-
-
-# A thread that consumes data
-def consumer(in_q):
-    while True:
-        # Get some data
-        dur, seed, move, evt = in_q.get()
-        start_pos = start_position()
-        # Process the data
-        play_sound(start_pos, dur)
-
-
-
-        # Indicate completion
-        evt.set()
-
-def start_position():
-
+def start_position(dur):
+    poss_length = int((audio_len - (dur * 2)) * 1000)
+    rnd_length_ms = randrange(poss_length)
+    return rnd_length_ms / 1000
 
 def play_sound(start_pos, dur):
-    play_sound = AudioSegment.from_file(song)
-    with play_lock:
-        play(play_sound)
-
-def threader():
-    while True:
-        song = q.get()
-        play_sound(song)
-        q.task_done()
-
-
+    start_pos_ms = start_pos * 1000
+    dur_ms = dur * 1000
+    end_pos_ms = start_pos_ms + dur_ms
+    audio_slice = audio[start_pos_ms : end_pos_ms]
+    play(audio_slice)
 
 def robot_move(pred_x, pred_y):
     multi_factor = 1
@@ -117,23 +93,50 @@ def robot_move(pred_x, pred_y):
     right_wheel_move += pred_y
     left_wheel_move += pred_y
     print(left_wheel_move, right_wheel_move)
-    robot.set_motors(left_wheel_move, right_wheel_move)
+    # robot.set_motors(left_wheel_move, right_wheel_move)
 
+# A thread that consumes data
+def consumer(in_q):
+    while True:
+        # Get some data
+        dur, seed, pred_x, pred_y, evt = in_q.get()
+        start_pos = start_position(dur)
 
+        # Process the data
+        robot_move(pred_x, pred_y)
+        play_sound(start_pos, dur) # in seconds!!!
 
+        # Indicate completion
+        evt.set()
 
 if __name__ == "__main__":
 
+    #user_dur = input("duration  ? ")
+    user_dur = 3 # minutes
     # instantiate the objects
     play_lock = Lock()
-    robot = Robot()
+    # robot = Robot()
+    running = True
+# todo multiple workers https://pythonprogramming.net/threading-tutorial-python/
 
     # Create the shared queue and launch both threads
     q = Queue()
+    worker_q = Queue()
+
+    now_time = time()
+    perf_dur = now_time + user_dur
+
+    # while time() < perf_dur:
+
     t1 = Thread(target=consumer, args=(q,))
-    t2 = Thread(target=producer_data, args=(q,))
+    t2 = Thread(target=consumer, args=(q,))
+    t3 = Thread(target=consumer, args=(q,))
+    t4 = Thread(target=producer_data, args=(q,))
+
     t1.start()
     t2.start()
+    t3.start()
+    t4.start()
 
     # Wait for all produced items to be consumed
     q.join()
--- main.py
+++ main.py
@@ -1,154 +1,218 @@
-from threading import Thread
-from random import randrange, random
-import data_engine as data
-import pandas as pd
-from glob import glob
+from threading import Thread, Lock, Event
+from queue import Queue
+from random import random, randrange
 from time import sleep, time
-import config
-from random import randrange
-from smoothing import DataAtoms, Data_Smoother
+import ML_robot_move
 
-# setup pathway parameters
-# dataset_path = glob('dataset/*')
-dataset_path = 'training/raw_phase1_dataset.csv'
-global_timer = True # todo make this user determined for duration of performance
-# global working_datafile_name
+play_lock = Lock()
 
-# get list of files from master dataset dir
-raw_files_list = dataset_path
-len_raw_files_list = len(raw_files_list)
+# The threader thread pulls an worker from the queue and processes it
+def get_threader():
+    while True:
+        # gets an worker from the queue
+        worker = q_get.get()
 
-print (raw_files_list, len_raw_files_list)
+        # Run the example job with the avail worker in queue (thread)
+        ML_robot_move.ml_predictions(worker)
 
+        # completed with the job
+        q_get.task_done()
 
-""" PLate spinning starts here"""
+def put_threader():
+    while True:
+        # gets an worker from the queue
+        worker = q_put.get()
 
-def timer_dataitems_seq():
-    while global_timer:
-        # loads in the current datafile and converts to dataframe for duration of this function
-        working_datafile_array = pd.read_csv(config.working_datafile_name, sep=",", header=None, names=["id", "limb", "x", "y", "z", "freq", "amp"])
+        # Run the example job with the avail worker in queue (thread)
+        ML_robot_move.producer_data(worker)
 
-        # random duration of datafile reading 3 -13 secs
-        dataitems_seq_read_dur = randrange(3000, 13000) / 1000
-        start_time_seq_read = time()  # calculates start time for each iteration
-        seq_read_end_time = start_time_seq_read + dataitems_seq_read_dur
+        # completed with the job
+        q_put.task_done()
 
-        # is it looped
-        is_loop_dur = data.is_loop() # returns with a 0 (no loop) or 0>1 (for loop & duration)
+# Create the queue and threader
+q_get = Queue()
+q_put = Queue()
 
-        # get starting point for read seq then
-        # divide into 2 processes: 1 if looped. 2 if not
-        working_dataitem_seq_start = data.read_start_point_from_datafile(working_datafile_array)
-        while time() < seq_read_end_time: # while current time is < duration of reading
-            if is_loop_dur > 0: # calculate current time and while that
-                loop_out_time = time() + is_loop_dur
-                loop_working_dataitem_seq_start = working_dataitem_seq_start # create a loop variable so can restart at top of loop
-                while time() < loop_out_time:  # for each loop iter pass the same start point to the parser
-                    print ('looping ')
-                    data.dataitem_parser(working_datafile_array, loop_working_dataitem_seq_start)
-                    loop_working_dataitem_seq_start += 1
-                    sleep(config.baudrate)  # todo BAUDRATE
-            else:
-                # working_dataitem_seq_start = data.read_start_point_from_datafile(working_datafile_array) # get new starting poitn
-                data.dataitem_parser(working_datafile_array, working_dataitem_seq_start)
-                print ('no loop')
-                working_dataitem_seq_start += 1
-                sleep(config.baudrate) # todo BAUDRATE
+# how many threads are we going to allow for
+for x in range(10):
+     t_get = Thread(target=get_threader)
+     t_put = Thread(target=put_threader)
 
-def baudrate(): # self iterating baudrate for dataset recalls
-    config.baudrate = randrange(300, 1300) / 1000
-    sleep(config.baudrate)
+     # classifying as a daemon, so they will die when the main dies
+     t_get.daemon = True
+     t_put.daemon = True
 
-def timer_datafile():
-# master data recall plate spinner = decided which datafile to use from the dataset
-    while global_timer:
-        datafile_dur = randrange(6000, 26000)/1000 # how long to read this file/ master loop?
-        print('how long to read this file = ', datafile_dur)
+     # begins, must come after daemon definition
+     t_get.start()
+     t_put.start()
 
-        rnd_data_list_pos = randrange(len_raw_files_list) # get a random file position from master set
-        print('random file position will be      ', rnd_data_list_pos)
+# 20 jobs assigned.
+for worker in range(20):
+    q_put.put(worker)
+    q_get.put(worker)
 
-        config.working_datafile_name = raw_files_list[rnd_data_list_pos] # feeds the chosen datafile to the global variable
-        print ('therefore I will be using this file for the moment     ', config.working_datafile_name)
-        sleep(datafile_dur) # todo change this to a while time() < statement to accomodate affect_interrupt_signal
-        #todo affect signal interrupt
-
-
-
-
-if __name__ == "__main__":
-    # smooth_bot = Data_Smoother()
-    # data_atoms_bot = DataAtoms()
-
-    t1 = Thread(target=timer_datafile) # controls the timing for master datafile choice
-    t2 = Thread(target=baudrate)
-    t3 = Thread(target=timer_dataitems_seq)
-    # t4 = Thread(target=data_atom_spinner)
-
-
-    t1.start()
-    t2.start()
-    sleep(0.1)
-    t3.start()
-    # smooth_bot.start()
-    # data_atoms_bot.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
+# wait until the thread terminates.
+q_put.join()
+q_get.join()
 
 
 
 
 
-# def timer(duration, start):  # manages the global timer for the performance
-#     global running
-#     while running:
-#         now = time()
-#         if start + duration >= now:
-#             running = False
-#             terminate()
-#             return False
-#         else:
-#             sleep(1)
+
+
+
+# from threading import Thread
+# from random import randrange, random
+# import data_engine as data
+# import pandas as pd
+# from glob import glob
+# from time import sleep, time
+# import config
+# from random import randrange
+# from smoothing import DataAtoms, Data_Smoother
 #
-# def terminate():
-#     data_bot.terminate()
+# # setup pathway parameters
+# # dataset_path = glob('dataset/*')
+# dataset_path = 'training/raw_phase1_dataset.csv'
+# global_timer = True # todo make this user determined for duration of performance
+# # global working_datafile_name
 #
-# def thought_train():  # sets up a thread that harvests new dataset for each random time period (6-26 seconds)
-#     while running:
-#         rnd_dur_train_thought = randrange(6, 26)
-#         data_bot.threader()
-#         sleep(rnd_dur_train_thought)
-#         data_bot.terminate()
+# # get list of files from master dataset dir
+# raw_files_list = dataset_path
+# len_raw_files_list = len(raw_files_list)
 #
-# def wheel_left():
-#     pass
+# print (raw_files_list, len_raw_files_list)
 #
-# def wheel_right():
-#     pass
 #
-# if __name__ == '__main__':
+# """ PLate spinning starts here"""
 #
-#     duration = input('duration in mins e.g. 6.5')
-#     duration = int(duration)
-#     start = time()
-#     data_bot = Engine()
-#     # todo a data_bot for each wheel
+# def timer_dataitems_seq():
+#     while global_timer:
+#         # loads in the current datafile and converts to dataframe for duration of this function
+#         working_datafile_array = pd.read_csv(config.working_datafile_name, sep=",", header=None, names=["id", "limb", "x", "y", "z", "freq", "amp"])
 #
-#     running = True
+#         # random duration of datafile reading 3 -13 secs
+#         dataitems_seq_read_dur = randrange(3000, 13000) / 1000
+#         start_time_seq_read = time()  # calculates start time for each iteration
+#         seq_read_end_time = start_time_seq_read + dataitems_seq_read_dur
 #
-#     t1 = Thread(target=timer, args=(duration, start))
-#     t2 = Thread(target=thought_train, daemon=True)
-#     t3 = Thread(target=wheel_left, daemon=True)
-#     t4 = Thread(target=wheel_right, daemon=True)
+#         # is it looped
+#         is_loop_dur = data.is_loop() # returns with a 0 (no loop) or 0>1 (for loop & duration)
+#
+#         # get starting point for read seq then
+#         # divide into 2 processes: 1 if looped. 2 if not
+#         working_dataitem_seq_start = data.read_start_point_from_datafile(working_datafile_array)
+#         while time() < seq_read_end_time: # while current time is < duration of reading
+#             if is_loop_dur > 0: # calculate current time and while that
+#                 loop_out_time = time() + is_loop_dur
+#                 loop_working_dataitem_seq_start = working_dataitem_seq_start # create a loop variable so can restart at top of loop
+#                 while time() < loop_out_time:  # for each loop iter pass the same start point to the parser
+#                     print ('looping ')
+#                     data.dataitem_parser(working_datafile_array, loop_working_dataitem_seq_start)
+#                     loop_working_dataitem_seq_start += 1
+#                     sleep(config.baudrate)  # todo BAUDRATE
+#             else:
+#                 # working_dataitem_seq_start = data.read_start_point_from_datafile(working_datafile_array) # get new starting poitn
+#                 data.dataitem_parser(working_datafile_array, working_dataitem_seq_start)
+#                 print ('no loop')
+#                 working_dataitem_seq_start += 1
+#                 sleep(config.baudrate) # todo BAUDRATE
+#
+# def baudrate(): # self iterating baudrate for dataset recalls
+#     config.baudrate = randrange(300, 1300) / 1000
+#     sleep(config.baudrate)
+#
+# def timer_datafile():
+# # master data recall plate spinner = decided which datafile to use from the dataset
+#     while global_timer:
+#         datafile_dur = randrange(6000, 26000)/1000 # how long to read this file/ master loop?
+#         print('how long to read this file = ', datafile_dur)
+#
+#         rnd_data_list_pos = randrange(len_raw_files_list) # get a random file position from master set
+#         print('random file position will be      ', rnd_data_list_pos)
+#
+#         config.working_datafile_name = raw_files_list[rnd_data_list_pos] # feeds the chosen datafile to the global variable
+#         print ('therefore I will be using this file for the moment     ', config.working_datafile_name)
+#         sleep(datafile_dur) # todo change this to a while time() < statement to accomodate affect_interrupt_signal
+#         #todo affect signal interrupt
+#
+#
+#
+#
+# if __name__ == "__main__":
+#     # smooth_bot = Data_Smoother()
+#     # data_atoms_bot = DataAtoms()
+#
+#     t1 = Thread(target=timer_datafile) # controls the timing for master datafile choice
+#     t2 = Thread(target=baudrate)
+#     t3 = Thread(target=timer_dataitems_seq)
+#     # t4 = Thread(target=data_atom_spinner)
+#
 #
 #     t1.start()
 #     t2.start()
+#     sleep(0.1)
 #     t3.start()
-#     t4.start()
+#     # smooth_bot.start()
+#     # data_atoms_bot.start()
 #
 #     t1.join()
 #     t2.join()
 #     t3.join()
-#     t4.join()
+#
+#
+#
+#
+#
+# # def timer(duration, start):  # manages the global timer for the performance
+# #     global running
+# #     while running:
+# #         now = time()
+# #         if start + duration >= now:
+# #             running = False
+# #             terminate()
+# #             return False
+# #         else:
+# #             sleep(1)
+# #
+# # def terminate():
+# #     data_bot.terminate()
+# #
+# # def thought_train():  # sets up a thread that harvests new dataset for each random time period (6-26 seconds)
+# #     while running:
+# #         rnd_dur_train_thought = randrange(6, 26)
+# #         data_bot.threader()
+# #         sleep(rnd_dur_train_thought)
+# #         data_bot.terminate()
+# #
+# # def wheel_left():
+# #     pass
+# #
+# # def wheel_right():
+# #     pass
+# #
+# # if __name__ == '__main__':
+# #
+# #     duration = input('duration in mins e.g. 6.5')
+# #     duration = int(duration)
+# #     start = time()
+# #     data_bot = Engine()
+# #     # todo a data_bot for each wheel
+# #
+# #     running = True
+# #
+# #     t1 = Thread(target=timer, args=(duration, start))
+# #     t2 = Thread(target=thought_train, daemon=True)
+# #     t3 = Thread(target=wheel_left, daemon=True)
+# #     t4 = Thread(target=wheel_right, daemon=True)
+# #
+# #     t1.start()
+# #     t2.start()
+# #     t3.start()
+# #     t4.start()
+# #
+# #     t1.join()
+# #     t2.join()
+# #     t3.join()
+# #     t4.join()
